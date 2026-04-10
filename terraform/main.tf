e9a1e5a71250634320f6a1b4886dda2133856c1a
Joshua-Lakra <joshualakra5@gmail.com>
2026-04-10 21:58:21 +0530

    Fix Terraform AMI filter block string pattern

--- terraform/main.tf
+++ terraform/main.tf
@@ -96,7 +96,7 @@ data "aws_ami" "ubuntu" {
   owners      = ["099720109477"] # Canonical
   filter {
     name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-22.04-amd64-server-*"]
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
   }
 }
 
